2f5770d4489b845bae2e1f353388e2acd15c9139
Wiz <im.not.ali@outlook.com>
2024-01-03 03:16:26 +0330

    Configure Project And Add Fonts And syles

---
+++ bloggroph/src/App.js
@@ -0,0 +1,22 @@
+import { Route, Routes } from "react-router";
+//components
+import HomePage from "./components/home/HomePage";
+import Layout from './components/layout/Layout';
+import BlogPage from './components/blog/BlogPage';
+import AuthorPage from './components/author/AuthorPage';
+import ScrollToTop from "./components/shared/ScrollToTop";
+
+function App() {
+  return (
+    <Layout>
+      <ScrollToTop/>
+       <Routes>
+         <Route path="/" element={<HomePage/>}/>
+         <Route path="/blogs/:slug" element={<BlogPage/>}/>
+         <Route path="/authors/:slug" element={<AuthorPage/>}/>
+       </Routes>
+    </Layout>
+  );
+}
+
+export default App;
